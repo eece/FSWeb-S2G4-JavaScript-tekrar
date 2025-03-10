4b4c6681f2e128bb60fdb0f6421518381d78cc72
eece <eece90@gmail.com>
2025-03-10 22:32:57 +0300

    Cemberin alani metodunu yazip calistir

--- index.js
+++ index.js
@@ -67,9 +67,10 @@ console.log(CemberinCevresi(5));
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap) {
+  return pi*Math.pow(yaricap,2);
 }
+console.log(CemberinAlani(15));
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
